89ca39c57107366204eedec811a3dd748c87bada
norbayeva@internet.ru <norbayeva@internet.ru>
2022-06-10 20:21:20 +0700

    лекция 5.3

--- addressbook_web_tests/addressbook_web_tests/appManager/ContactHelper.cs
+++ addressbook_web_tests/addressbook_web_tests/appManager/ContactHelper.cs
@@ -20,6 +20,7 @@ namespace WebAddressbookTests
             : base(manager)
         {
         }
+
         public ContactHelper CreateContact(ContactData contact)
         {
             InitNewContactCreation();
@@ -127,10 +128,50 @@ namespace WebAddressbookTests
                     {
 
                         Id = element.FindElement(By.TagName("input")).GetAttribute("value"),
-                        Lastname = element.FindElement(By.XPath("./ td[2]")).Text
+                        Lastname = element.FindElement(By.XPath("./td[2]")).Text
                     });
                 }
             }           return new List<ContactData>(contactCache);
+        }
+
+        public ContactData GetContactInformationFromTable(int index)
+        {
+            manager.Navi.ClickHomeButton();
+            IList<IWebElement> cells = driver.FindElements(By.Name("entry"))[index]
+                 .FindElements(By.TagName("td"));
+            string lastName = cells[1].Text;
+            string firstName = cells[2].Text;
+            string address = cells[3].Text;
+            string allPhones = cells[5].Text;
+
+            return new ContactData(firstName)
+            {
+                Lastname = lastName,
+                Address = address,
+                AllPhones = allPhones
+            };
+        }
+
+        public ContactData GetContactInformationFromEditForm(int index)
+        {
+            manager.Navi.ClickHomeButton();
+                        SelectModifyContact(0);
+            string firstName = driver.FindElement(By.Name("firstname")).GetAttribute("value");
+            string lastName = driver.FindElement(By.Name("lastname")).GetAttribute("value");
+            string address = driver.FindElement(By.Name("address")).GetAttribute("value");
+            string homePhone = driver.FindElement(By.Name("home")).GetAttribute("value");
+            string mobilePhone = driver.FindElement(By.Name("mobile")).GetAttribute("value");
+            string workPhone = driver.FindElement(By.Name("work")).GetAttribute("value");
+
+            return new ContactData(firstName)
+            {
+                Lastname = lastName,
+                Address = address,
+                HomePhone = homePhone,
+                MobilePhone = mobilePhone,
+                WorkPhone = workPhone
+            };
+
         }
     }
 }
--- addressbook_web_tests/addressbook_web_tests/model/ContactData.cs
+++ addressbook_web_tests/addressbook_web_tests/model/ContactData.cs
@@ -14,6 +14,7 @@ namespace WebAddressbookTests
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
+        private string allPhones;
         public ContactData(string firstname)
         {
             Firstname = firstname;
@@ -50,7 +51,35 @@ namespace WebAddressbookTests
         public string Firstname { get; set; }
         public string Middlename { get; set; }
         public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string WorkPhone { get; set; }
         public string Id { get; internal set; }
 
+        public string AllPhones
+        {
+            get
+            {
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone)).Trim();
+                }
+            }
+            set { allPhones = value; }
+        }
+
+        private string CleanUp(string phone)
+        {
+            if (phone == null || phone == "")
+            {
+                return "";
+            }
+            return phone.Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "") + "\r\n";
+        }
     }
 }
